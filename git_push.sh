162bcbe345914e20ed90bc9d2f7d4161289ac275
office <17630984562@163.com>
2020-07-29 21:03:21 +0800

    2020年 07月 29日 星期三 21:03:13 CST

--- git_push.sh
+++ git_push.sh
@@ -4,10 +4,8 @@ GITPATH=/home/garfield/文档
 ADD=$(git add .)
 DATE=$(date)
 GITPUSH=$(git push blog master)
-GITPULL=$(git pull blog master)
 cd $GITPATH
 $ADD 
 git add img/
 git commit -m "$DATE"
-$GITPULL
 $GITPUSH
